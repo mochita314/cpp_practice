c66c2585ab9f31e3048149aa9044e0cdfd6c6c72
鬼澤英里 <kizawa-eri@g.ecc.u-tokyo.ac.jp>
2019-12-19 23:00:54 +0900

    1219

---
+++ prob1_3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "ONE TWO THREE" << endl << "FOUR FIVE SIX" << endl;
+}
---
+++ prob1_4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "数字を入力してください:";
+    int num;
+    cin >> num;
+    cout << num << "を2倍した数は、" << 2*num <<"です。\n";
+}
